b965f69b684c2256f9925186ef6ac08c7c8d9c7c
Lisa Jonsson <jonsson.lisa@gmail.com>
2014-07-09 12:19:36 -0700

    categories

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 PostitTemplate::Application.routes.draw do
   root to: 'posts#index'
-  resources :posts, except:  [:destroy] do  
-    resources :comments, only: [:create, :show]
-  end
+  resources :posts, except:  [:destroy]  
+  resources :comments, only: [:create, :show]
+  resources :categories, only: [:new, :create, :show]
+
 end
