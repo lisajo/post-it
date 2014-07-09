59bebeebb22d13f1970b193d5f3354c457812cf0
Lisa Jonsson <jonsson.lisa@gmail.com>
2014-07-08 18:49:37 -0700

    add new and edit posts forms

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,17 +5,43 @@ class PostsController < ApplicationController
 
   def show
     @post = Post.find(params[:id])
+    @comments = Comment.all
   end
 
   def new
+    @post = Post.new
+  end
 
+  def create
+    @post = Post.new(post_params)
+    @post.user = User.first
+    
+      if @post.save
+        flash[:notice] = "Your post was created."
+        redirect_to posts_path
+      else
+        render :new
+      end
   end
 
   def edit
+    @post = Post.find(params[:id])
   end
 
   def update
+    @post = Post.find(params[:id])
+
+    if @post.update(post_params)
+      flash[:notice] = "Your post has been updated."
+      redirect_to post_path(@post)
+    else
+      render :edit
+    end
   end
 
+  private
 
+  def post_params
+    params.require(:post).permit(:title, :url, :description)
+  end
 end
