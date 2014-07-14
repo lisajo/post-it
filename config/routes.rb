8a15c3b214af2136816c52917c23f68f491bbac5
Lisa Jonsson <jonsson.lisa@gmail.com>
2014-07-13 17:56:29 -0700

    comments form

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 PostitTemplate::Application.routes.draw do
   root to: 'posts#index'
-  resources :posts, except:  [:destroy]  
-  resources :comments, only: [:create, :show]
+  resources :posts, except:  [:destroy] do 
+    resources :comments, only: [:create, :show]
+  end
   resources :categories, only: [:new, :create, :show]
+
 
 end
