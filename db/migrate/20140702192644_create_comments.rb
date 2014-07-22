5c91f349a770750ff630971a6c72b5bcb248508d
Lisa Jonsson <jonsson.lisa@gmail.com>
2014-07-21 23:15:22 -0700

    validations for user reg

--- db/migrate/20140702192644_create_comments.rb
+++ db/migrate/20140702192644_create_comments.rb
@@ -4,11 +4,7 @@ class CreateComments < ActiveRecord::Migration
     	t.text :body
     	 t.integer :user_id, :post_id
     	 t.timestamps
-    	 add_column :comments, :body, :text
-    	 add_column :comments, :user_id, :integer
-    	 add_column :comments, :post_id, :integer
-    	 add_column :comments, :created_at, :datetime
-    	 add_column :comments, :updated_at, :datetime
+    	 
     end
   end
 end
