105719da412f00400ea5ca856527391fe76a70e5
Lisa Jonsson <jonsson.lisa@gmail.com>
2014-08-01 16:03:13 -0700

    users show view

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,4 +13,10 @@ class ApplicationController < ActionController::Base
     !!current_user
   end
 
+  def require_user
+    if !logged_in?
+      flash[:error] = "You must log in first."
+      redirect_to root_path
+    end
+  end
 end
